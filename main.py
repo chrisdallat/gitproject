4659575ec87ad84e6bfbd2a2aec04fea2227d889
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-05-05 17:27:22 +0100

    fixed main function and added find_patron find_book method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 class Book:
     def __init__(self, title, author, publication_year, ISBN):
         self.title = title
@@ -65,9 +66,49 @@ class Library:
             if search_string.lower() in book.title.lower() or search_string.lower() in book.author.lower():
                 matching_books.append(book)
         return matching_books
+    
+    def find_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        return None
+
+    def find_patron(self, name):
+        for patron in self.patrons:
+            if patron.name == name:
+                return patron
+        return None
+
 
 def main():
-   print("Hello from main!")
+    # Create some books and patrons
+    book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "978-0-7432-7356-5")
+    book2 = Book("To Kill a Mockingbird", "Harper Lee", 1960, "0-446-31078-6")
+    book3 = Book("Pride and Prejudice", "Jane Austen", 1813, "978-0-14-143951-8")
+    patron1 = Patron("Alice")
+    patron2 = Patron("Bob")
+    
+    # Create the library and add the books and patrons to it
+    library = Library([book1, book2, book3], [patron1, patron2])
+
+    # Parse command line arguments
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <patron name> <book title>")
+        print("Use \"quotations\" for patron name and book title!")
+        return
+    
+    patron_name = sys.argv[1]
+    book_title = sys.argv[2]
+
+    # Find the patron and book objects in the library
+    patron = library.find_patron(patron_name)
+    book = library.find_book(book_title)
+
+    # Rent the book to the patron
+    if patron.checkout(book):
+        print(f"{book_title} checked out by {patron_name}")
+    else:
+        print(f"{book_title} is already checked out")
 
 if __name__ == "__main__":
    main()
